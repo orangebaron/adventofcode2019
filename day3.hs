6df9a2f3f01cbdcf68e65216029ea19ac4a3e693
Sam Alws <sam.alws@gmail.com>
2019-12-03 23:56:45 -0600

    day 3 part 2

--- day3.hs
+++ day3.hs
@@ -11,6 +11,12 @@ addPos p1 p2 = Pos (x p1 + x p2) (y p1 + y p2)
 manhattan :: Pos -> Int
 manhattan (Pos a b) = (abs a) + (abs b)
 
+negPos :: Pos -> Pos
+negPos p = Pos (- x p) (- y p)
+
+manhattanDiff :: Pos -> Pos -> Int
+manhattanDiff p1 p2 = manhattan $ p1 `addPos` negPos p2
+
 insideOnAxis :: Bool -> Pos -> Wire -> Bool
 insideOnAxis axisBool a (Wire b c) = f (axis a) (axis b) (axis c) where
 	axis = if axisBool then y else x
@@ -55,4 +61,30 @@ main1 s = show $ bestIntersection (l 0) (l 1) where
 	l :: Int -> [Wire]
 	l = (makeWires (Pos 0 0)) . (splitOn ",") . (lines s !!)
 
-main = interact main1
+makeWiresWithDistances :: Pos -> Int -> [String] -> [(Wire, Int)]
+makeWiresWithDistances _ _ [] = []
+makeWiresWithDistances p traveledSoFar (s:rest) = ((Wire p p2), traveledSoFar):(makeWiresWithDistances p2 (traveledSoFar + dist) rest) where
+	offset ('R':n) = Pos (read n) 0
+	offset ('L':n) = Pos (-read n) 0
+	offset ('U':n) = Pos 0 (read n)
+	offset ('D':n) = Pos 0 (-read n)
+	p2 = p `addPos` offset s
+	dist = read $ drop 1 s
+
+intersectionsWithDistances :: [(Wire, Int)] -> [(Wire, Int)] -> [Int]
+intersectionsWithDistances l1 l2 = catMaybes $ do
+	(w1, d1) <- l1
+	(w2, d2) <- l2
+	return $ do
+		p <- intersecting w1 w2
+		return $ d1 + d2 + (fstPos w1 `manhattanDiff` p) + (fstPos w2 `manhattanDiff` p)
+
+bestIntersectionByDistance :: [(Wire, Int)] -> [(Wire, Int)] -> Int
+bestIntersectionByDistance l1 l2 = head $ dropZeroIfHead $ sort $ intersectionsWithDistances l1 l2
+
+main2 :: String -> String
+main2 s = show $ bestIntersectionByDistance (l 0) (l 1) where
+	l :: Int -> [(Wire, Int)]
+	l = (makeWiresWithDistances (Pos 0 0) 0) . (splitOn ",") . (lines s !!)
+
+main = interact main2
